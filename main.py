6fbaa832eb5bb26a0f61d49a0f566bf2feff0927
S <dchavezcu@unsa.edu.pe>
2026-09-10 15:10:46 -0500

    Parametros base acordados en el equipo

--- main.py
+++ main.py
@@ -4,3 +4,8 @@ print("Curso: Métodos Numéricos")
 print("Equipo: Paul (P), Renato (M), Massiel (D) y Ronald (V)")
 print("======================================================\n")
 
+#Parametros base acordados por el equipo 
+tolerancia = 0.000001
+max_iteraciones = 100
+x_inicial_newton = 1.5
+intervalo_biseccion = [1.0, 2.0]
