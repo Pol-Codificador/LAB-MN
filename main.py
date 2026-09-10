6cd884477aa329d48d071be4e969c8cfcbe3c012
S <dchavezcu@unsa.edu.pe>
2026-09-10 15:07:10 -0500

    Se agrega el archivo main.py donde se desarrollara el presente laboratorio, primero creamos el encabezado

---
+++ main.py
@@ -0,0 +1,6 @@
+print("======================================================")
+print("Módulo de Ecuaciones No Lineales - Prototipo Inicial")
+print("Curso: Métodos Numéricos")
+print("Equipo: Paul (P), Renato (M), Massiel (D) y Ronald (V)")
+print("======================================================\n")
+
